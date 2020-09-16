7f9e0071edc20e698610d4aeb1ae7ba3e432a736
Cesar Preza <cpreza93@yahoo.com>
2020-09-15 21:11:20 -0700

    styling and input field functionality when empty

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let button = document.getElementById("taskButton");
 button.addEventListener('click', () => taskList());
 
+
 function taskList() {
   let item = document.getElementById("taskInput").value;
   let newTask = document.createElement("li");
   let deleteTaskButton = document.createElement("button");
-  deleteTaskButton.textContent = "delete " + item;
+  deleteTaskButton.textContent = "Delete";
   newTask.innerText = item;
   document.getElementById("taskLister").appendChild(newTask);
   newTask.appendChild(deleteTaskButton);
@@ -13,10 +14,14 @@ function taskList() {
   if(markTaskComplete) {
     newTask.addEventListener('click', (e) => markTaskComplete(e));
     deleteTaskButton.addEventListener('click', (i) => removeTask(i));
-  }
+  } 
+  if(item == '') {
+    alert('Please enter a task.');
+    return false;
+  } 
 }
   
- 
+
 function markTaskComplete(e) {
   let strikeThrough = e.target;
   strikeThrough.style.textDecoration = "line-through";
@@ -27,6 +32,10 @@ function removeTask(i) {
   let deleteTask = i.target.parentElement;
   deleteTask.remove();
 }
+
+
+
+
 
 
 
